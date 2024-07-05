bd59fc14fd93a74625d9dfe7d56ea78bd1b851d4
Harold Guzman <Gzmn.h10195@gmail.com>
2024-07-06 05:46:07 +0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import VideoList from "./Video_list.svelte";
+	export let name;
+	let people = [
+		{name:"mark", age:30, id:1},
+		{name:"joe", age:28, id:2},
+		{name:"ron-ron", age:18, id:3},
+		{name:"harold", age:18, id:3}
+	];
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+{#each people as person}
+	<VideoList namse = "{person.name}" />
+{/each}
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Video_list.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let namse = "adawd"
+</script>
+
+
+<div class="cardvideo">
+     <div class="video_display"></div>
+     <div class="sidecontent"></div>
+</div>
+
+
+<style>
+
+.cardvideo{
+    width:250px;
+    height: 320px;
+    border: 1px solid black;
+    display: inline-block;
+    margin-right:10px;
+    border-radius: 10px;
+    position: relative;
+}
+
+.cardvideo > .video_display{
+    background-color: gray;
+    height:100%;
+    width:100%;
+}
+
+.cardvideo > .sidecontent{
+    
+}
+
+</style>
